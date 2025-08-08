5b7388e188f11cb6c17f05f437a3043c0815e60b
woodrowdemo <woodrowdemo@gmail.com>
2025-08-07 23:14:25 -0400

    Update project structure and add HomePage class
    - Updated `PageObjectModelPW.csproj` to include a `testcases` folder and target .NET 8.0.
    - Removed `UnitTest1.cs`, eliminating the previous test structure.
    - Added `HomePage` class in `HomePage.cs` with methods for web interactions using Playwright.

---
+++ pages/HomePage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+
+namespace PageObjectModelPW.pages
+{
+    internal class HomePage
+    {
+
+        IPage page;
+        public async void FindNewCars( )
+        {
+            await page.Locator("//div[normalize-space()='New Cars']").HoverAsync();
+            await page.Locator("//div[normalize-space()='Find New Cars']").ClickAsync();
+        }
+
+        public async Task SearchCars( )
+        {
+            await page.Locator("//input[@placeholder='Search']").FillAsync("BMW");
+        }
+
+        public void GoToPopularCars( )
+        {
+
+        }
+
+        public void GoToUpComingCars( )
+        {
+
+        }
+
+
+    }
+}
