970fdf5c448b7949dbeb02f747bac0de1f3dda66
woodrowdemo <woodrowdemo@gmail.com>
2025-09-16 21:48:03 -0400

    Add reporting and configuration setup for tests
    - Updated `PageObjectModelPW.csproj` to include a `resources` folder.
    - Added `AventStack.ExtentReports` for report generation in `BaseTest.cs`.
    - Introduced `OneTimeSetUp` and `OneTimeTearDown` methods for logging and report management.
    - Implemented `CreateInstance` for configuring `ExtentReports`.
    - Added `BeforeEachTest` for initializing tests and Playwright.
    - Created `CaptureScreenshot` method for screenshot handling.
    - Added new `appsettings.json` for application configuration settings.

--- testcases/BaseTest.cs
+++ testcases/BaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Config;
 
 using log4net;
 
@@ -18,11 +20,72 @@ namespace PageObjectModelPW.testcases
         private static readonly ILog log = LogManager.GetLogger(typeof(BaseTest));
 
         IConfiguration configuration;
+        private static string fileName;
 
+
+        [OneTimeSetUp]
         public void OneTimeSetUp( )
         {
+            log.Info("Test Execution Started!!!");
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\")
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            DateTime currentTime = DateTime.Now;
+            string fileName = "Extent_" + currentTime.ToString("yyyyMMdd_HHmmss") + ".html";
+            extent = CreateInstance(fileName);
+
 
         }
 
+        public static ExtentReports CreateInstance(string filename)
+        {
+
+
+            var htmlReporter = new ExtentSparkReporter(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName);
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.DocumentTitle = "Test Artifacts";
+            htmlReporter.Config.ReportName = "Test Results";
+            htmlReporter.Config.Encoding = "utf-8";
+
+            extent.AddSystemInfo("Executed by:", "Woodrow Winters");
+            extent.AddSystemInfo("Organization:", "Woodrows Extent Report Demo");
+            extent.AddSystemInfo("Build No :", "W2A-1234");
+
+
+            return extent;
+
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown( )
+        {
+            extent.Flush();
+            log.Info("Test Execution Completed!!!");
+
+
+        }
+
+        [SetUp]
+        public async void BeforeEachTest( )
+        {
+
+            test = extent.CreateTest($"{TestContext.CurrentContext.Test.ClassName} - {TestContext.CurrentContext.Test.Name}");
+            playwright = await Playwright.CreateAsync();
+        }
+
+        public static void CaptureScreenshot(IPage page)
+        {
+            DateTime currentTime = DateTime.Now;
+            fileName = currentTime.ToString("yyyy-MM-dd_HHmmss") + ".jpg";
+        }
+
+        [TearDown]
+        public void AfterEachTest( )
+        {
+
+        }
     }
 }
