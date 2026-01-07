817780ccddf8c419e6bce6876fa9a978d875027a
woodrowdemo <woodrowdemo@gmail.com>
2026-01-06 20:56:42 -0500

    Add car name and price test with Excel data source
    Introduced CarNameAndPriceTest to automate validation of car names and prices for multiple brands using Playwright and NUnit. Added GetCarNameAndPrices method to CarBase for extracting and printing car details. Updated TestSpreadSheet.xlsx to provide test data, and implemented error handling and resource cleanup in tests.

--- pages/CarBase.cs
+++ pages/CarBase.cs
@@ -6,12 +6,23 @@ namespace PageObjectModelPW.pages
     {
         IPage page;
 
+        //car name - //div/div/div/div/a/h3
+        //car price -//div/div/div[3]/div/span/span[1]
+
         public CarBase(IPage page) => this.page = page;
 
         public async Task<string> GetCarTitle( )
         {
             return await BasePage.keyword.GetText("CarBase", "cartitle");
             //return await page.Locator("//header/h1").InnerTextAsync();
+        }
+
+        public async Task GetCarNameAndPrices( )
+        {
+            for (int i = 0; i < await page.Locator("//div/div/div[3]/div/span/span[1]").CountAsync(); i++)
+            {
+                Console.WriteLine(await page.Locator("//div/div/div/div/a/h3").Nth(i).InnerTextAsync() + "------price is : " + await page.Locator("//div/div/div[3]/div/span/span[1]").Nth(i).InnerTextAsync());
+            }
         }
     }
 }
---
+++ testcases/CarNameAndPriceTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Playwright;
+
+using PageObjectModelPW.pages;
+using PageObjectModelPW.utilities;
+
+namespace PageObjectModelPW.testcases
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    internal class CarNameAndPriceTest : BaseTest
+    {
+        [Parallelizable(ParallelScope.Self)]
+        [Test, TestCaseSource(nameof(GetTestData))]
+        public async Task CarNameAndPrice(string carbrand, string browserType, string runmode)
+        {
+
+            if (runmode.Equals("N"))
+            {
+
+                Assert.Ignore("Ignoring the test as the run mode is No.");
+            }
+
+            // Each test gets a new playwright instance
+            using var playwrightInstance = await Playwright.CreateAsync();
+
+            var (browser, page) = await CreateBrowserAndPage(playwrightInstance, browserType, new BrowserTypeLaunchOptions { Headless = false });
+
+            HomePage homePage = new HomePage(page);
+            NewCarsPage newCar = await homePage.FindNewCars();
+
+            var carBrandsActions = new Dictionary<string, Func<Task>>
+                {
+                    { "bmw", newCar.GoToBMW },
+                    { "honda", newCar.GoToHonda },
+                    { "toyota", newCar.GoToToyota },
+                    { "mg", newCar.GoToMG },
+
+                };
+            try
+            {
+
+
+                if (carBrandsActions.TryGetValue(carbrand.ToLower(), out var navigateToCar))
+                {
+
+                    await navigateToCar();
+                    await BasePage.carBase.GetCarNameAndPrices();
+                }
+                else
+                {
+                    Assert.Fail($"Car brand '{carbrand}' does not exist");
+
+                }
+
+                await Task.Delay(2000);
+            }
+
+            catch (Exception)
+            {
+                await CaptureScreenshot(page);
+
+            }
+            finally
+            {
+                await page.CloseAsync();
+                await browser.CloseAsync();
+            }
+        }
+        public static IEnumerable<TestCaseData> GetTestData( )
+        {
+            var columns = new List<string> { "carbrand", "browserType", "runmode" };
+
+            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\TestSpreadSheet.xlsx", "CarNameAndPrice", columns);
+        }
+
+    }
+}
