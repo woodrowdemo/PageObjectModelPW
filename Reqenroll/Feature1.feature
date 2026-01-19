12d365c66825eda9eff866172d17699177aac42d
woodrowdemo <woodrowdemo@gmail.com>
2026-01-18 22:53:26 -0500

    Add BDD test skeleton for SauceDemo login with Reqnroll
    Added step definitions and feature file for invalid login scenario on SauceDemo using Reqnroll. Updated project to include folders for test assets and outputs.

---
+++ Reqenroll/Feature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: SauceLogin Page
+
+This is to test the login page for sauce demo page
+
+
+Scenario: Testing Logging into the SauceDemo Page with invalid credentials
+	Given I navigate to the SauceDemo login page
+	When I enter invalid username and password
+	Then I should see an error message indicating invalid credentials
---
+++ SauceLoginPageStepDefinitions.cs
@@ -0,0 +1,26 @@
+using Reqnroll;
+
+namespace PageObjectModelPW
+{
+    [Binding]
+    public class SauceLoginPageStepDefinitions
+    {
+        [Given("I navigate to the SauceDemo login page")]
+        public void GivenINavigateToTheSauceDemoLoginPage( )
+        {
+
+        }
+
+        [When("I enter invalid username and password")]
+        public void WhenIEnterInvalidUsernameAndPassword( )
+        {
+
+        }
+
+        [Then("I should see an error message indicating invalid credentials")]
+        public void ThenIShouldSeeAnErrorMessageIndicatingInvalidCredentials( )
+        {
+
+        }
+    }
+}
