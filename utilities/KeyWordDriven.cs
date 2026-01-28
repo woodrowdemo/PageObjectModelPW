3b634eff9d43b37a265ad11e199c13ecca22b8f5
woodrowdemo <woodrowdemo@gmail.com>
2026-01-27 20:38:55 -0500

    Revert "Enhance locator resolution and add self-healing support"
    This reverts commit df33354f48acd62a924636d632fd6c4acf95fade.

--- utilities/KeyWordDriven.cs
+++ utilities/KeyWordDriven.cs
@@ -23,129 +23,56 @@ namespace PageObjectModelPW.TestCases
 
         public async Task Type(string pageName, string locatorName, string value)
         {
-            BaseTest.test.Info("Typing into element : " + locatorName + " value: " + value);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                // Try RepairAgent with the pageName and locatorName as fallback hint
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.FillAsync(value);
+            BaseTest.test.Info("Getting text from the element : " + locatorName + " entered the value as : " + value);
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).FillAsync(value);
         }
 
         public async Task Click(string pageName, string locatorName)
         {
             BaseTest.test.Info("Clicking on an element : " + locatorName);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.ClickAsync();
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).ClickAsync();
         }
 
         public async Task MouseOver(string pageName, string locatorName)
         {
             BaseTest.test.Info("Hovering over element : " + locatorName);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.HoverAsync();
+            await _page.HoverAsync(XMLLocatorReader.GetLocatorValue(pageName, locatorName));
         }
 
         public async Task<string> GetText(string pageName, string locatorName)
         {
             BaseTest.test.Info("Getting text from the element: " + locatorName);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            return await locator.InnerTextAsync();
+            return await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).InnerTextAsync();
         }
 
         public async Task<bool> IsVisible(string pageName, string locatorName)
         {
             BaseTest.test.Info("Checking visibility of element: " + locatorName);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) return false;
-            return await locator.IsVisibleAsync();
+            return await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).IsVisibleAsync();
         }
 
         public async Task SelectDropdownByValue(string pageName, string locatorName, string value)
         {
             BaseTest.test.Info($"Selecting value '{value}' from dropdown: {locatorName}");
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.SelectOptionAsync(new SelectOptionValue { Value = value });
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).SelectOptionAsync(new SelectOptionValue { Value = value });
         }
 
         public async Task WaitForElement(string pageName, string locatorName, int timeoutMs = 5000)
         {
             BaseTest.test.Info($"Waiting for element: {locatorName}");
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.WaitForAsync(new LocatorWaitForOptions { Timeout = timeoutMs });
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).WaitForAsync(new LocatorWaitForOptions { Timeout = timeoutMs });
         }
 
         public async Task UploadFile(string pageName, string locatorName, string filePath)
         {
             BaseTest.test.Info($"Uploading file '{filePath}' to element: {locatorName}");
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.SetInputFilesAsync(filePath);
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).SetInputFilesAsync(filePath);
         }
 
         public async Task Clear(string pageName, string locatorName)
         {
             BaseTest.test.Info("Clearing value of element: " + locatorName);
-            var locator = await LocatorResolver.ResolveAsync(_page, pageName, locatorName);
-            if (locator == null)
-            {
-                var alt = await RepairAgent.FindAlternativeAsync(_page, pageName, locatorName);
-                if (!string.IsNullOrEmpty(alt)) locator = _page.Locator(alt);
-            }
-
-            if (locator == null) throw new Exception($"Unable to resolve locator for {pageName}.{locatorName}");
-            await locator.FillAsync(string.Empty);
+            await _page.Locator(XMLLocatorReader.GetLocatorValue(pageName, locatorName)).FillAsync(string.Empty);
         }
     }
 }
